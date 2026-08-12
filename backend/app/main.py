6bbfcabec4f7b1befcea2f8e8213b28ad99d877b
Sayantan De <desayan384@gmail.com>
2026-08-12 12:56:35 +0530

    Allow deployed frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,12 +27,12 @@ app = FastAPI(
 # ==============================
 # CORS CONFIGURATION
 # ==============================
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://expenseflow-4bjg.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
